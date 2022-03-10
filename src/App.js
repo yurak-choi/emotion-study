a9449a3d9e50bbb11758e6b03e7eefe2a005cb1c
yurak-choi <nozo8026@gmail.com>
2022-03-10 23:03:21 +0900

    first commit for emotion study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react'
+
+const style = css`
+  color: aqua;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div css={style}>emotion study</div>
     </div>
   );
 }
